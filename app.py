8a8959bb0013d71737e160ccae6cb082b2a06e6d
AFASFSAFAFDWRW <144069740+AFASFSAFAFDWRW@users.noreply.github.com>
2026-02-03 07:15:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -143,6 +143,7 @@ if __name__ == '__main__':
     # Запускаем бота в отдельном потоке внутри Flask
     threading.Thread(target=run_bot, daemon=True).start()
     
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
 
